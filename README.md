50dd79ce96cbd31b27fcfa904a885589f3f4e1fa
kanoemon <kanoemon.cho@gmail.com>
2021-01-21 14:56:42 +0900

    Insert data

---
+++ README.md
@@ -0,0 +1,8 @@
+## Migrations
+```
+# create a seed file
+$ npx sequelize-cli seed:generate --name demo-user
+
+# run migrations
+$ npx sequelize-cli db:seed:all
+```
---
+++ src/seeders/20210121040147-insert-data-categories-sizes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('categories', [
+      {name: 'drink'},
+      {name: 'food'}
+    ]);
+
+    await queryInterface.bulkInsert('sizes', [
+      {name: 'short'},
+      {name: 'tall'},
+      {name: 'grande'},
+      {name: 'vendi'}
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
